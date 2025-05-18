d6516c74bcfe10605cf13cba2071f7b6184791a4
batistacaceres <batistacaceres@gmail.com>
2025-05-18 20:16:52 +0200

    adicione Google Tag Manager

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,14 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
+import TagManager from "react-gtm-module";
+
+// Inicializa GTM
+const tagManagerArgs = {
+  gtmId: "GTM-PGWW43Z9", // tu GTM ID
+};
+
+TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
